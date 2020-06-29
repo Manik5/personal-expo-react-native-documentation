32ab47c72061fe428e683e5925d4624b938738eb
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-06-30 00:31:32 +0200

    first commit

---
+++ simple-blog/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import IndexScreen from './src/screen/IndexScreen';
+import ShowScreen from './src/screen/ShowScreen';
+import CreateScreen from './src/screen/CreateScreen';
+import { Provider } from './src/context/BlogContext';
+
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+	Show: ShowScreen,
+	Create: CreateScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
+});
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <Provider>
+      <App />
+    </Provider>
+  )
+};
---
+++ simple-blog/src/context/BlogContext.js
@@ -0,0 +1,42 @@
+
+import createDataContext from './createDataContext';
+
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_BLOG_POST':
+      return state.filter((blogPost) => blogPost.id !== action.payload );
+    case 'ADD_BLOG_POST':
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: action.payload.title,
+          content: action.payload.content
+        }
+      ];
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = (dispatch) => {
+  return (title, content) => {
+    dispatch({
+      type: 'ADD_BLOG_POST',
+      payload: { title, content}
+    })
+  }
+};
+
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({ type: 'DELETE_BLOG_POST', payload: id})
+  }
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost },
+  []
+);
