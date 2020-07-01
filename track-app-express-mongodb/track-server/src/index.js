9622028524f0bebf3b2c580d0e4eebd3a83bd3e9
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-07-01 17:55:30 +0200

    implementing trackroute

--- track-app-express-mongodb/track-server/src/index.js
+++ track-app-express-mongodb/track-server/src/index.js
@@ -1,16 +1,19 @@
 require('./models/User');
+require('./models/Track');
+
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
-
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://admin:adminpassword@cluster0.f4cn5.mongodb.net/<dbname>?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
