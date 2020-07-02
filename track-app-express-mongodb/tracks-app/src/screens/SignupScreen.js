b4130ffc67e2d650bccd8c4fb9cda17f6a30db46
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-07-02 17:35:41 +0200

    defined signup component

--- track-app-express-mongodb/tracks-app/src/screens/SignupScreen.js
+++ track-app-express-mongodb/tracks-app/src/screens/SignupScreen.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-  return(
-    <>
-      <Text>SignupScreen</Text>
-      <Button
-        title="Go to Signin"
-        onPress={() => navigation.navigate('Signin')}
-      />
-      <Button
-        title="Go to Main Flow"
-        onPress={() => navigation.navigate('mainFlow')}
-      />
-    </>
-  )
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+        <Input label="Email" />
+        <Spacer />
+        <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 150
+  }
 });
 
 export default SignupScreen;
