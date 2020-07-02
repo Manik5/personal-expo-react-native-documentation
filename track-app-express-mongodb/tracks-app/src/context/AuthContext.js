1bd94a1afafdc1b01baf14156ffc85936d84cd11
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-07-02 19:01:42 +0200

    set up axios and ngrok

--- track-app-express-mongodb/tracks-app/src/context/AuthContext.js
+++ track-app-express-mongodb/tracks-app/src/context/AuthContext.js
@@ -2,18 +2,44 @@ import createDataContext from './createDataContext';
 
 
 const authReducer = (state, action) => {
-	switch (action.type) {
-		case value:
+  switch (action.type) {
+    case value:
 
-			break;
+      break;
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
+};
+
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+
+    // if we sign up, modify ur state and say that we are authenticated
+
+    // if signing up fails, we need to reflect an error message somewhere
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signin
+
+    // Handle success by updating state
+
+    // Handle failure by showing error message
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // somehow sign out
+  };
 };
 
 export const { Provider, Context } = createDataContext(
-	authReducer,
-	{},
-	{ isSignedIn: false }
+  authReducer,
+  {signin, signout, signup},
+  { isSignedIn: false }
 );
