388256c03f7b8ff7e68a148392d54c6e2af4265a
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-07-03 17:06:46 +0200

    small changes

--- track-app-express-mongodb/tracks-app/App.js
+++ track-app-express-mongodb/tracks-app/App.js
@@ -10,12 +10,14 @@ import SignupScreen from './src/screens/SignupScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+import InitialScreen from './src/screens/InitialScreen';
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
 
 import { setNavigator } from './src/navigationRef';
 
 const switchNavigator = createSwitchNavigator({
+  Initial: InitialScreen,
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
     Signin: SigninScreen
